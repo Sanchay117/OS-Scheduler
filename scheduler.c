26dcafcf2b52edfab44647555dda5f8dbe6e9459
Sanchay117 <146355316+Sanchay117@users.noreply.github.com>
2024-10-29 15:11:57 +0530

    Add files via upload

--- scheduler.c
+++ scheduler.c
@@ -1,14 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #define SCHEDULER_PIPE "/tmp/scheduler_pipe" // The same pipe name
+#define MAX_SIZE 250
 
-void start_scheduler(int n,int slice) {
+int front = 0,rear = 0;
+int num_CPU,TSLICE;
+
+pid_t ready_queue[MAX_SIZE];
+
+void enqueue(pid_t pid) {
+    if ((rear + 1) % MAX_SIZE == front) {
+        printf("Queue is full\n");
+
+        if(kill(pid,SIGKILL)==-1){
+            printf("Couldn't kill Process with PID:%d\n",pid);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Process with PID:%d\n Killed",pid);
+
+        return;
+    }
+    ready_queue[rear] = pid;
+    rear = (rear + 1) % MAX_SIZE;
+}
+
+pid_t dequeue() {
+    if (front == rear) {
+        printf("Queue is empty\n");
+        return -1;
+    }
+    pid_t pid = ready_queue[front];
+    front = (front + 1) % MAX_SIZE;
+    return pid;
+}
+
+
+void start_scheduler() {
     mkfifo(SCHEDULER_PIPE, 0666); // Create named pipe if it doesn’t exist
 
     while (1) {
@@ -31,8 +67,8 @@ void start_scheduler(int n,int slice) {
 
 int main(int argc,char** argv) {
 
-    int num_CPU = atoi(argv[1]);
-    int TSLICE = atoi(argv[2]);
+    num_CPU = atoi(argv[1]);
+    TSLICE = atoi(argv[2]);
 
     start_scheduler(num_CPU,TSLICE);
     return 0;
